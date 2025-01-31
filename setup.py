32c72b86af087449fdb659d569d0ef04ce774e9d
Rqwannn <mohwannn1@gmail.com>
2025-01-31 13:23:15 +0700

    feat: tearm 2 projek

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Chest-Cancer-Classification-Project"
+REPO_NAME = "End-to-End-Chest-Cancer-Classification-using-MLflow-DVC"
 AUTHOR_USER_NAME = "rqwannn"
 SRC_REPO = "cnnClassifier"
 AUTHOR_EMAIL = "rqwannn@gmail.com"
